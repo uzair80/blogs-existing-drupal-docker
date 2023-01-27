7fe149711e249335d867436472de09c00fb8157b
uzair <uzair21180@gmail.com>
2023-01-27 23:59:32 +0500

    new changes 23

---
+++ themes/custom/chatting/js/global.js
@@ -0,0 +1,25 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function ($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.chatting = {
+    attach: function (context, settings) {
+
+
+      /**
+       * Modal Form
+       * 
+       * 
+       */
+
+
+
+    }
+  };
+
+})(jQuery, Drupal);
